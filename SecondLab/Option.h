47350fbd046c387929ac520c2181e7b4312109ac
Linuxovichock <m@dema.ru>
2025-06-22 22:54:50 +0300

    Option

--- SecondLab/Option.h
+++ SecondLab/Option.h
@@ -1,1 +1,27 @@
 #pragma once
+
+#include "Error.h"
+
+template <typename T>
+class Option {
+private:
+    bool hasValue;
+    T value;
+public:
+    Option() : hasValue(false) {}
+    Option(const T& val) : hasValue(true), value(val) {}
+
+    static Option<T> Some(const T& val) { return Option<T>(val); }
+    static Option<T> None() { return Option<T>(); }
+
+    bool IsSome() const { return hasValue; }
+    bool IsNone() const { return !hasValue; }
+
+    T Unwrap() const {
+        if (!hasValue) throw Errors[RUNTIME_ERROR].message;
+        return value;
+    }
+    T UnwrapOr(const T& defaultVal) const {
+        return hasValue ? value : defaultVal;
+    }
+};
